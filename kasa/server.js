c1cef2710ff52df33a1f232654d074059470c1e2
tom007373 <d.pstudio@wp.pl>
2026-07-08 19:32:16 +0200

    rail-git1?

---
+++ kasa/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Pliki statyczne
+app.use(express.static(path.join(__dirname, "public")));
+
+// Strony HTML
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server działa na porcie ${PORT}`);
+});
