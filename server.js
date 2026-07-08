17246e5320efe3981662187b76b09b8cd65e9f3b
tom007373 <d.pstudio@wp.pl>
2026-07-08 19:48:31 +0200

    git1

--- server.js
+++ server.js
@@ -9,18 +9,13 @@ app.use(express.urlencoded({ extended: true }));
 // Pliki statyczne
 app.use(express.static(path.join(__dirname, "public")));
 
-// Strony HTML
+// Strona główna
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "pierwsza_próba_home.html"));
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server działa na porcie ${PORT}`);
-});
-const path = require("path");
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "pierwsza_próba_home.html"));
 });
