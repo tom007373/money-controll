1dd7a95acaeb7ce46022a53a15e4ecb36701372c
tom007373 <d.pstudio@wp.pl>
2026-07-09 16:30:21 +0200

    db_v0.1

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const pool = require("./db");
 const express = require("express");
 const path = require("path");
 
@@ -15,6 +16,15 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
+pool.query("SELECT NOW()")
+    .then(res => {
+        console.log("Połączono z bazą!");
+        console.log(res.rows);
+    })
+    .catch(err => {
+        console.error(err);
+    });
 
 app.listen(PORT, () => {
     console.log(`Server działa na porcie ${PORT}`);
