c942ef265702952fe32eeeb3be34e789cd1706a1
tom007373 <d.pstudio@wp.pl>
2026-07-10 00:43:24 +0200

    db_v0.3

--- server.js
+++ server.js
@@ -7,25 +7,43 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Pliki statyczne
 app.use(express.static(path.join(__dirname, "public")));
 
-// Strona główna
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "pierwsza_próba_home.html"));
 });
 
 const PORT = process.env.PORT || 3000;
 
-pool.query("SELECT NOW()")
-    .then(res => {
+async function initDB() {
+    try {
+
+        // Sprawdzenie połączenia
+        const res = await pool.query("SELECT NOW()");
         console.log("Połączono z bazą!");
         console.log(res.rows);
-    })
-    .catch(err => {
-        console.error(err);
-    });
 
-app.listen(PORT, () => {
-    console.log(`Server działa na porcie ${PORT}`);
+        // Tworzenie tabeli
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS users(
+                id SERIAL PRIMARY KEY,
+                imie VARCHAR(50) NOT NULL,
+                nazwisko VARCHAR(50) NOT NULL,
+                email VARCHAR(100) UNIQUE NOT NULL,
+                haslo TEXT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `);
+
+        console.log("Tabela users gotowa.");
+
+    } catch(err){
+        console.error(err);
+    }
+}
+
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server działa na porcie ${PORT}`);
+    });
 });
